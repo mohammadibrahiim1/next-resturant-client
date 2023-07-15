3090e631ec85842c920d54dfb393255b65c7ec2c
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-07-16 00:46:34 +0600

    updated

--- src/Context/DataContext.js
+++ src/Context/DataContext.js
@@ -3,9 +3,33 @@ export const ApiContext = createContext();
 
 const DataContext = ({ children }) => {
   const [cart, setCart] = useState([]);
+  console.log(cart);
 
-  const addToCart = () => {
-    console.log("add to cart");
+  const addToCart = async (item) => {
+    try {
+      await fetch("http://localhost:5000/api/v1/cart", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          item,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.acknowledged) {
+            window.alert("saved successfully");
+          } else {
+            console.error(error.message);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+
+    setCart(item);
   };
   const info = {
     addToCart,
